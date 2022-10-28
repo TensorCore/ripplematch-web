b4aed1315a104027375262d76cf46d6046aa358b
TensorCore <shakerad0109@gmail.com>
2022-10-27 23:14:01 -0700

    Fixed Logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,7 @@ const Navbar = () => {
             <div className="navbar-start">
                 <a href='https://ripplematch.com/index?r=RjM6MT' target="_blank" rel="noopener noreferrer">
                     <div className="btn btn-ghost normal-case lg:text-2xl text-xl md:btn-wide lg:btn-wide">
-                        <span>RIPPLE</span>
-                        <span className="text-primary">MATCH</span>
+                        <Image src="/svg/ripplematch-logo.svg" height="50" width="250" alt="Image" />
                     </div>
                 </a>
             </div>
@@ -20,7 +19,7 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a href='https://ripplematch.com/index?r=RjM6MT' target="_blank" rel="noopener noreferrer" className="btn btn-outline btn-primary text-primary-content hover:btn-primary-focus lg:btn-wide">Get Started</a>
+                <a href='https://ripplematch.com/index?r=RjM6MT' target="_blank" rel="noopener noreferrer" className="btn btn-outline btn-primary text-primary-content hover:btn-primary-focus lg:btn-wide hidden sm:flex">Get Started</a>
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
